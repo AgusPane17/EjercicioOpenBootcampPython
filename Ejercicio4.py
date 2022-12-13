b8907b4c62ef3b7d1b141d466fc1705a976828cf
Agustin Panella <apanella17@gmail.com>
2022-12-12 21:53:03 -0300

    Correccion de error

--- Ejercicio4.py
+++ Ejercicio4.py
@@ -1,6 +1,4 @@
 num = 100
 while num != 0:
-    print("El número de la cuenta regreesiva es: ", num)
-    num -= 1
-
-print("Hola mundo")
+        print("El número de la cuenta regreesiva es: ", num)
+        num -= 1
