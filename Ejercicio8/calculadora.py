5c9df6adb8331a373c0bc3053b9dea816753be81
Agustin Panella <apanella17@gmail.com>
2022-12-23 23:43:39 -0300

    Ejercicio numero 1 de modulos

---
+++ Ejercicio8/calculadora.py
@@ -0,0 +1,9 @@
+import funciones.operaciones
+
+def calculadora():
+    print(funciones.operaciones.suma(1, 5))
+    print(funciones.operaciones.resta(1, 5))
+    print(funciones.operaciones.multi(1, 5))
+    print(funciones.operaciones.div(1, 5))
+
+calculadora()
---
+++ Ejercicio8/funciones/operaciones.py
@@ -0,0 +1,17 @@
+
+
+def suma(a, b):
+
+    return a + b
+def resta(a, b):
+   
+    return a - b
+
+def multi(a, b):
+
+    return a * b
+
+def div(a, b):
+
+   if (b!=0):
+    return a / b
