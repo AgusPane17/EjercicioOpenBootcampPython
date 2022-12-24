04010642d33ebb7f981a056a7ea57c79725bf27b
Agustin Panella <apanella17@gmail.com>
2022-12-24 03:03:44 -0300

    Ejercicio numero 2 de modulos en python corregido

--- Ejercicio9/irseACasa.py
+++ Ejercicio9/irseACasa.py
@@ -3,12 +3,18 @@ import time
 horaActual = time.strftime("%H:%M:%S")
 
 horaActualInt = int(horaActual.split(":")[0]) #esta line conviete la horas a enteros
-
-if horaActualInt >= 19:
+horaActualInt = 5
+if horaActualInt == 19:
     print("Es la hora de irse")
 
-else:
-    horaRestante = 19 - horaActualInt
-    minRestantes = (60- int(horaActual.split(":")[1])) % 60
+elif ((horaActualInt> 8) & (horaActualInt < 19)):
+        horaRestante = 19 - horaActualInt
+        print("xd")
+        minRestantes = (60- int(horaActual.split(":")[1])) % 60
 
-    print(f"Quedan {horaRestante} horas y {minRestantes} para poder salir del trabajo")
+        print(f"Quedan {horaRestante} horas y {minRestantes} para poder salir del trabajo")
+elif (horaActualInt>19):
+        print(f"Faltan {(24-horaActualInt)+8} hs para tu siguiente jornada laborar a las 8")
+
+else:
+    print(f"Te quedan {8-horaActualInt} horas para tu proxima jornada")
